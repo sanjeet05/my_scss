06fafd7f27d36d83997c831ba4d85fc4b60c3508
sanjeet.k <kumarsanjeet1990@gmail.com>
2017-06-24 21:50:26 +0530

    wip

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,27 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css/'))
+// variables
+var input = 'sass/**/*.scss';
+var output = './css/';
+
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
+
+// sass task
+gulp.task('sass', function () {
+  return gulp
+    // Find all `.scss` files from the `stylesheets/` folder
+    .src(input)
+    // Run Sass on those files
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    // Write the resulting CSS in the output folder
+    .pipe(gulp.dest(output));
 });
 
-//Watch task
+// Watch task
 gulp.task('default',function() {
     gulp.watch('sass/**/*.scss',['styles']);
 });
